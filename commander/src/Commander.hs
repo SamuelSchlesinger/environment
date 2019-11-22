86aee47eef929ece08bd59e6c8a55fbee8cc835a
Samuel Schlesinger <samuel@simspace.com>
2019-11-22 01:01:36 -0500

    Polished up Commander a bit

--- commander/src/Commander.hs
+++ commander/src/Commander.hs
@@ -6,15 +6,15 @@
 {-# LANGUAGE DataKinds #-}
 module Commander where
 
-import Control.Applicative
-import Control.Monad
-import Control.Monad.Trans
+import Control.Applicative (Alternative(..))
+import Control.Monad (ap, void)
+import Control.Monad.Trans (MonadIO(..))
 import Data.HashSet (HashSet)
 import Data.HashMap.Strict as HashMap
-import Data.Proxy
-import Data.Text (Text, pack, unpack)
+import Data.Proxy (Proxy(..))
+import Data.Text (Text, pack)
 import GHC.TypeLits (Symbol, KnownSymbol, symbolVal)
-import System.Environment
+import System.Environment (getArgs)
 
 data Arg :: Symbol -> * -> *
 
@@ -33,7 +33,7 @@ data a + b
 infixr 2 +
 
 class Unrender t where
-  unrender :: Text -> t
+  unrender :: Text -> Maybe t
 
 data CommanderT summary state m a
   = Action (state -> m (CommanderT summary state m a, state, summary))
@@ -45,14 +45,15 @@ summaryAction :: (Monoid summary, Monad m) => summary -> CommanderT summary stat
 summaryAction summary (Defeat summary') = Defeat (summary <> summary')
 summaryAction summary (Victory summary' a) = Victory (summary <> summary') a
 summaryAction summary (Action action) = Action \state -> action state >>= \case
-  (action', state', summary') -> return (action', state', summary <> summary')
+  (action', state', summary') -> return (summaryAction summary action', state', summary')
 
-runCommanderT :: (Monoid summary, Monad m) => state -> CommanderT summary state m a -> m (summary, Maybe a)
-runCommanderT state (Action action) = do
+runCommanderT :: (Monoid summary, Monad m) => CommanderT summary state m a -> state -> m (summary, Maybe a)
+runCommanderT (Action action) state = do
   (action', state', summary) <- action state
-  runCommanderT state' (summaryAction summary action')
-runCommanderT _ (Defeat summary) = return (summary, Nothing)
-runCommanderT _ (Victory summary a) = return (summary, Just a)
+  (summary', m) <- runCommanderT action' state'
+  return (summary <> summary', m)
+runCommanderT (Defeat summary) _ = return (summary, Nothing)
+runCommanderT (Victory summary a) _ = return (summary, Just a)
 
 instance (Monad m, Monoid summary) => Applicative (CommanderT summary state m) where
   (<*>) = ap
@@ -90,66 +91,67 @@ data State = State
   , options :: HashMap Text Text
   , flags :: HashSet Text }
 
+data Event 
+  = BadOption Text Text
+  | BadArgument Text
+  | WrongBranch Text
+  | Success
+
 class HasProgram p where
   data ProgramT p (m :: * -> *)
-  run :: ProgramT p IO -> CommanderT [Text] State IO ()
+  run :: ProgramT p IO -> CommanderT [Event] State IO ()
+  hoist :: (forall x. m x -> n x) -> ProgramT p m -> ProgramT p n 
 
 instance (Unrender t, KnownSymbol name, HasProgram p) => HasProgram (Arg name t ... p) where
   newtype ProgramT (Arg name t ... p) m = ArgProgramT { unArgProgramT :: t -> ProgramT p m }
   run f = Action $ \State{..} -> do
     case arguments of
-      (x : xs) -> do
-        return (run (unArgProgramT f $ unrender x), State{ arguments = xs, .. }, mempty)  
+      (x : xs) -> 
+        case unrender x of
+          Just t -> return (run (unArgProgramT f t), State{ arguments = xs, .. }, mempty)  
+          Nothing -> return (Defeat [BadArgument (pack $ symbolVal (Proxy @name))], State{..}, mempty)
       [] -> return (Defeat mempty, State{..}, mempty)
+  hoist n (ArgProgramT f) = ArgProgramT (hoist n . f)
 
 instance (HasProgram x, HasProgram y) => HasProgram (x + y) where
   data ProgramT (x + y) m = ProgramT x m :+: ProgramT y m
   run (f :+: g) = run f <|> run g
+  hoist n (f :+: g) = hoist n f :+: hoist n g
 
 infixr 2 :+:
 
 instance HasProgram Raw where
   newtype ProgramT Raw m = RawProgramT { unRawProgramT :: m () }
   run m = liftIO $ unRawProgramT m
+  hoist n (RawProgramT m) = RawProgramT (n m)
 
 instance (KnownSymbol long, KnownSymbol short, HasProgram p, Unrender (Maybe t)) => HasProgram (Opt long short t ... p) where
   newtype ProgramT (Opt long short t ... p) m = OptProgramT { unOptProgramT :: Maybe t -> ProgramT p m }
   run f = Action $ \State{..} -> do
     case HashMap.lookup (pack $ symbolVal (Proxy @long)) options <|> HashMap.lookup (pack $ symbolVal (Proxy @short)) options of
-      Just opt -> return (run (unOptProgramT f . unrender $ opt), State{..}, mempty)
+      Just opt -> 
+        case unrender opt of
+          Just t -> return (run (unOptProgramT f t), State{..}, mempty)
+          Nothing -> return (Defeat [BadOption (pack (symbolVal $ Proxy @short)) (pack (symbolVal $ Proxy @long))], State{..}, mempty)
       Nothing  -> return (run (unOptProgramT f Nothing), State{..}, mempty)
+  hoist n (OptProgramT f) = OptProgramT (hoist n . f)
 
 instance HasProgram p => HasProgram (Doc doc ... p) where
   newtype ProgramT (Doc doc ...p) m = DocProgramT { unDocProgramT :: ProgramT p m }
   run = run . unDocProgramT 
+  hoist n = DocProgramT . hoist n . unDocProgramT
 
 instance (KnownSymbol seg, HasProgram p) => HasProgram (seg ... p) where
   newtype ProgramT (seg ... p) m = SegProgramT { unSegProgramT :: ProgramT p m }
   run s = Action $ \State{..} -> do 
     case arguments of
       (x : xs) -> if x == pack (symbolVal $ Proxy @seg) then return (run $ unSegProgramT s, State{arguments = xs, ..}, mempty)
-                                                        else return (Defeat mempty, State{..}, [pack $ symbolVal $ Proxy @seg])
-      [] -> return (Defeat mempty, State{..}, [pack $ symbolVal $ Proxy @seg])
-
-type Interface = "booyah" ... Opt "--poop" "-p" Bool ... Raw
-               + "hooray" ... Arg "plankton"  String ... Raw
-               + Raw
-    
-example :: ProgramT Interface IO
-example = example1 :+: example0 :+: help
-  where
-    example0 = SegProgramT . ArgProgramT $ \t -> RawProgramT $ case t of
-           "Boooop" -> putStrLn "Boooop"
-           "Yeeeah-dawg" -> putStrLn "Heyo"
-           _ -> putStrLn "Wut?"
-    example1 = SegProgramT . OptProgramT $ \t -> RawProgramT $ case t of
-      Just True -> putStrLn "CheesyBoyz"
-      Just False -> putStrLn "REEEEE"
-      Nothing -> putStrLn "..."
-    help = RawProgramT $ putStrLn "YOU DON'T GET NO HELP"
-
-getInitialState :: IO State
-getInitialState = do
+                                                        else return (Defeat mempty, State{..}, [WrongBranch $ pack $ symbolVal $ Proxy @seg])
+      [] -> return (Defeat mempty, State{..}, [WrongBranch $ pack $ symbolVal $ Proxy @seg])
+  hoist n = SegProgramT . hoist n . unSegProgramT
+ 
+initialState :: IO State
+initialState = do
   args <- getArgs
   let (opts, args') = takeOptions args
   return $ State args' (HashMap.fromList opts) mempty 
@@ -161,13 +163,5 @@ getInitialState = do
         go opts args (x : y) = go opts (pack x : args) y
         go opts args [] = (opts, reverse args)
 
-instance Unrender String where
-  unrender = unpack
-
-instance Unrender (Maybe Bool) where
-  unrender "True" = Just True
-  unrender "False" = Just False
-  unrender _ = Nothing  
-
-main :: IO ()
-main = void $ liftIO getInitialState >>= flip runCommanderT (run example)
+commander :: HasProgram p => ProgramT p IO -> IO ()
+commander prog = void $ initialState >>= runCommanderT (run prog)
