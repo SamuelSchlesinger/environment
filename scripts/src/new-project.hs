2ab77b5916fab2693e23492c5beb8e3e7f55db46
Samuel Schlesinger <samuel@simspace.com>
2019-11-23 17:05:35 -0500

    made new-project better, modding scripts is way easier now :thumbsup

--- scripts/src/new-project.hs
+++ scripts/src/new-project.hs
@@ -4,14 +4,16 @@ import System.Process
 import System.Environment
 
 main :: IO ()
-main = command_ . toplevel @"new-project" $ arg @"project-name" \projectName -> raw do
-  currentDir <- getCurrentDirectory
-  env <- getEnv "ENVIRONMENT_BASE_PATH"
-  let projectDir = currentDir <> "/" <> projectName
-  callProcess "cp" ["-R", env ++ "/project-template", projectDir]
-  setCurrentDirectory projectDir
-  callProcess "git" ["init"]
-  callProcess "git" ["add", "."]
-  callProcess "git" ["checkout", "-b", "dev"]
-  callProcess "git" ["commit", "-m", "Genesis block"]
-  
+main = command_ . toplevel @"new-project" 
+  $ arg @"project-name" \projectName ->
+    opt @"initial-branch-name" @"branch" @"b" \maybeBranchName -> raw do
+    let branchName = maybe "dev" id maybeBranchName
+    currentDir <- getCurrentDirectory
+    env <- getEnv "ENVIRONMENT_BASE_PATH"
+    let projectDir = currentDir <> "/" <> projectName
+    callProcess "cp" ["-R", env ++ "/project-template", projectDir]
+    setCurrentDirectory projectDir
+    callProcess "git" ["init"]
+    callProcess "git" ["add", "."]
+    callProcess "git" ["checkout", "-b", branchName]
+    callProcess "git" ["commit", "-m", "Genesis block"]
