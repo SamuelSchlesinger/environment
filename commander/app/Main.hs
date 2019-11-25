d1570c1f13a3fc5bac815899f3f441fdd0683471
Samuel Schlesinger <samuel@simspace.com>
2019-11-25 08:35:48 -0500

    Made README better and changed example

--- commander/app/Main.hs
+++ commander/app/Main.hs
@@ -3,12 +3,13 @@ module Main where
 import Commander 
 import Prelude
 
-type File = Arg "filename" FilePath 
+type File = Named "file"
+          & Arg "filename" FilePath 
           & ("write" & Arg "contents" String & Raw
           +  "read"  & Raw) 
 
 file :: ProgramT File IO ()
-file = arg \a -> (sub $ arg (raw . writeFile a)) :+: (sub . raw $ readFile a >>= putStrLn)
+file = named $ arg \a -> (sub $ arg (raw . writeFile a)) :+: (sub . raw $ readFile a >>= putStrLn)
 
 main :: IO ()
-main = command_ (toplevel @"commander-example" file)
+main = command_ (file :+: usage @File)
